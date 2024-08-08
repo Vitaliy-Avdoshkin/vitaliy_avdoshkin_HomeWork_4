bc1e6103e1cfd01390a7e0408d4386469e693bfa
Vutaliy.Avdoshkin <wioswa@gmail.com>
2024-08-08 23:07:25 +0300

    module main is created

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,42 @@
 import pytest
-from src.main import *
+
+from src.main import Product, Category
+
 
 @pytest.fixture()
 def product_samsung():
-    return Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+    return Product(
+        "Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5
+    )
 
-product1 = 1
-product2 = 2
-product3 = 3
+
+@pytest.fixture()
+def product_iphone():
+    return Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+
+
+@pytest.fixture()
+def product_xiaomi():
+    return Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+
 
 @pytest.fixture()
 def category1():
-    return Category("Смартфоны", "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",[product1, product2, product3])
+    product1 = 1
+    product2 = 2
+    product3 = 3
+    return Category(
+        "Смартфоны",
+        "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+        [product1, product2, product3],
+    )
+
+
+@pytest.fixture()
+def category2():
+    product4 = 4
+    return Category(
+        "Телевизоры",
+        "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником",
+        [product4],
+    )
