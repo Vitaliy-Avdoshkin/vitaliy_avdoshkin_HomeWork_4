c988e2f15b13ce9685d9d0beceaaf242f14b1f48
Vutaliy.Avdoshkin <wioswa@gmail.com>
2024-08-08 20:22:00 +0300

    module main is created

--- src/main.py
+++ src/main.py
@@ -24,7 +24,7 @@ class Category:
         self.description = description
         self.products = products
         Category.category_count += 1
-        Category.product_count += 1
+        Category.product_count += len(products)
 
 
 if __name__ == "__main__":
