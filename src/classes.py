6898ebc0816215ae5636e7aee4072dde28fd99c0
Vutaliy.Avdoshkin <wioswa@gmail.com>
2024-08-12 22:42:38 +0300

    module "14.2" add in project

--- src/classes.py
+++ src/classes.py
@@ -24,9 +24,12 @@ class Category:
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
         Category.category_count += 1
         Category.product_count += len(products)
+
+    def add_product(self):
+
 
 
 if __name__ == "__main__":
