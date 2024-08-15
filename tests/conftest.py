20e6336ab65cef09f530990254f15ff5f9756d70
Vutaliy.Avdoshkin <wioswa@gmail.com>
2024-08-15 14:47:35 +0300

    HW_16.1 is DONE

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,7 @@
 import pytest
 
 from src.cls_category import Category
-from src.cls_product import Product
+from src.cls_product import LawnGrass, Product, Smartphone
 from src.cls_product_iterator import ProductIterator
 
 
@@ -55,3 +55,51 @@ def new_product():
 @pytest.fixture
 def product_iterator(category1):
     return ProductIterator(category1)
+
+
+@pytest.fixture()
+def smartphone1():
+    return Smartphone(
+        "Samsung Galaxy S23 Ultra",
+        "256GB, Серый цвет, 200MP камера",
+        180000.0,
+        5,
+        95.5,
+        "S23 Ultra",
+        256,
+        "Серый",
+    )
+
+
+@pytest.fixture()
+def smartphone2():
+    return Smartphone(
+        "Iphone 15", "512GB, Gray space", 210000.0, 8, 98.2, "15", 512, "Gray space"
+    )
+
+
+@pytest.fixture()
+def smartphone3():
+    return Smartphone(
+        "Xiaomi Redmi Note 11",
+        "1024GB, Синий",
+        31000.0,
+        14,
+        90.3,
+        "Note 11",
+        1024,
+        "Синий",
+    )
+
+
+@pytest.fixture()
+def grass1():
+    return LawnGrass(
+        "Газонная трава",
+        "Элитная трава для газона",
+        500.0,
+        20,
+        "Россия",
+        "7 дней",
+        "Зеленый",
+    )
